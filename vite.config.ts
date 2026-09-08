7afbaa86649d65d76ac9260034717b8add769ea8
cleartechnologysolutions <adam@cleartechnologysolutions.com>
2026-09-07 21:29:03 -0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "77d2043c-a133-4c6a-8a84-62f3de23e30e";
 
 const d1 = "DB";
 const r2 = null;
